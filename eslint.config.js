9eacf4bb16d6b16832d2fdc0e9205184d791d2ea
Craig Banach <craigsb93@gmail.com>
2025-06-19 21:09:32 +0100

    Use prettier for formatting alongside eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,7 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,8 +13,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+);
